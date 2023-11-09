83a99aaae105b01f6276371063b252697dbdcd30
Yashwanthdasari <133797558+Yashwanthdasari@users.noreply.github.com>
2023-11-09 11:33:05 +0530

    Update dropdown1.java

--- src/test/java/Dropdowns/dropdown1.java
+++ src/test/java/Dropdowns/dropdown1.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 //yeah!....The Test is success..
+//hello yashwanth------>>>>>>>>>>>>> pull operation was done 
 public class dropdown1 {
 
 	public static void main(String[] args) throws InterruptedException {
